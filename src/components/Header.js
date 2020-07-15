4db7bf36a48cd911f50f4b3a2ca1d480677f3d55
Murilo Roque <muriloengqui@gmail.com>
2020-07-15 17:30:40 -0300

    add Header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import '../styles/Header.scss';
-import logo from '../assets/images/murilo.png'
-import resume from '../assets/Murilo Roque - Resume.pdf'
+import logo from '../assets/images/murilo.png';
+import resume from '../assets/Murilo Roque - Resume.pdf';
+import github from '../assets/images/github.png';
+import linkedin from '../assets/images/linkedin.png';
+import twitter from '../assets/images/twitter.png';
+import mail from '../assets/images/mail.png';
+import resumeImg from '../assets/images/resume.png';
 
 const Header = () => (
   <header>
@@ -9,11 +14,21 @@ const Header = () => (
       <img src={logo} alt="logo"/>
     </div>
     <div className='header-right'>
-      <a href={"https://github.com/MuriloRoque"} target={'_blank'}>GitHub</a>
-      <a href={"https://www.linkedin.com/in/murilo-roque/"} target={'_blank'}>LinkedIn</a>
-      <a href={"https://twitter.com/MuriloRoquePai1"} target={'_blank'}>Twitter</a>
-      <a href={"%mailto: muriloengqui@gmail.com"} target={'_blank'}>E-mail</a>
-      <a href={resume} target={'_blank'}>Resume</a>
+      <a href={"https://github.com/MuriloRoque"} target={'_blank'}>
+        <img src={github} alt="github"/>
+        GitHub</a>
+      <a href={"https://www.linkedin.com/in/murilo-roque/"} target={'_blank'}>
+        <img src={linkedin} alt="linkedin"/>
+        LinkedIn</a>
+      <a href={"https://twitter.com/MuriloRoquePai1"} target={'_blank'}>
+        <img src={twitter} alt="twitter"/>
+        Twitter</a>
+      <a href={"mailto: muriloengqui@gmail.com"}>
+        <img src={mail} alt="mail"/>
+        E-mail</a>
+      <a href={resume} target={'_blank'}>
+        <img src={resumeImg} alt="resume"/>
+        Resume</a>
     </div>
   </header>
 );
