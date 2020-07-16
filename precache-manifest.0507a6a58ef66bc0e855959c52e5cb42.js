8ed8248ee836b082a3174e7b3b22cd43812cdad1
Murilo Roque <muriloengqui@gmail.com>
2020-07-16 13:16:55 -0300

    Updates

---
+++ precache-manifest.0507a6a58ef66bc0e855959c52e5cb42.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102ebf944dbf5b4a495029afed48ecf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc50ef476a078bd675d6",
+    "url": "/static/css/main.4f27cca5.chunk.css"
+  },
+  {
+    "revision": "43307abcdacf2088bc83",
+    "url": "/static/js/2.ba2576ac.chunk.js"
+  },
+  {
+    "revision": "229313a91dfc1a4229b4758368bd8914",
+    "url": "/static/js/2.ba2576ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc50ef476a078bd675d6",
+    "url": "/static/js/main.83efa3aa.chunk.js"
+  },
+  {
+    "revision": "bf979d705cc3532e6630",
+    "url": "/static/js/runtime-main.f5cc3951.js"
+  },
+  {
+    "revision": "9a9eeddb3eb9ce4f64e378a8a7b9c042",
+    "url": "/static/media/Inter-Regular.9a9eeddb.otf"
+  },
+  {
+    "revision": "8e884942021f71e59e8adffddb34da84",
+    "url": "/static/media/Murilo Roque - Resume.8e884942.pdf"
+  },
+  {
+    "revision": "7118eab5dd71ba5628820864007c500c",
+    "url": "/static/media/chemisearch.7118eab5.png"
+  },
+  {
+    "revision": "1e0f824d8739106345d54cb7537bfc68",
+    "url": "/static/media/food-tracker.1e0f824d.png"
+  },
+  {
+    "revision": "c309d71cb2e0198b645eeea3035a915d",
+    "url": "/static/media/handshake.c309d71c.png"
+  },
+  {
+    "revision": "d0bdbe535f2c210f25c55e268d067cfd",
+    "url": "/static/media/hello.d0bdbe53.png"
+  },
+  {
+    "revision": "b3ab4b7b1647303db96407f973c807de",
+    "url": "/static/media/martha.b3ab4b7b.gif"
+  },
+  {
+    "revision": "d8bdb9bc90a86ddbb8263e3c8c2a8b31",
+    "url": "/static/media/resume.d8bdb9bc.png"
+  },
+  {
+    "revision": "dc362f827c1880d1d88079b26bee6c29",
+    "url": "/static/media/twitter-ss.dc362f82.png"
+  }
+]);
